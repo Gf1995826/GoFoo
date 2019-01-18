980c9118a239db36bbf016427c882454d9496f58
gaofeng <feng.gao@cienet.com.cn>
2019-01-18 18:32:17 +0800

    Test for HelloWorld By use springBoot

---
+++ springboot01helloworld/src/main/java/com/cienet/HelloWorldMainApplication.java
@@ -0,0 +1,22 @@
+package com.cienet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @SpringBootApplication This is a spring boot application
+ */
+@SpringBootApplication
+public class HelloWorldMainApplication {
+
+
+    //psvm
+    public static void main(String[] args) {
+
+        /**
+         * Spring Application start
+         */
+        SpringApplication.run(HelloWorldMainApplication.class,args);
+    }
+
+}
